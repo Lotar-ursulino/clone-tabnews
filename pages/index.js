9fb9b3bc67b651867ae888bf04a30fdead25bec8
Lotar-ursudev <145760350+Lotar-ursulino@users.noreply.github.com>
2025-03-16 22:32:49 +0000

    mudança na frase

--- pages/index.js
+++ pages/index.js
@@ -10,5 +10,4 @@ function Agu(){
     return <h1>Agu, vc é a melhor coisa que aconteceu na minha vida, vc é minha filha, minha tudo, te amo muito minha filha!!!!</h1>
 }
 
-export default Home
-export {Amor, Agu}
+export {Home, Amor, Agu};
