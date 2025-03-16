91cc4da395cc2b2cf5aa8c0a0c9aab8f2004aa25
Lotar-ursudev <145760350+Lotar-ursulino@users.noreply.github.com>
2025-03-16 22:49:05 +0000

    mudando

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     var straing1 = "Amor,Agu, Amo muito vcs dua!!!!";
     var straing2 = "Amor,eu amo muito voce, quero continuar construindo uma vida inteira com vc, sou muito feliz ao seu lado meu amor";
     var straing3 = "Filha, eu te amo muito, vc é a razão da minha vida, voce e meu tudo, amo muito vc";
-    return {String1:straing1, String2:straing2, String3:straing3};      
+    return {straing1, straing2, straing3};      
     
 }
  export default Home;
