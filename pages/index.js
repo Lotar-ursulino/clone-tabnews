a7455274f431d21a504a392d61fa641215115c66
Lotar-ursudev <145760350+Lotar-ursulino@users.noreply.github.com>
2025-03-16 22:29:53 +0000

    frases

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
     return <h1>Amor,Agu, Amo muito vcs duas!!!!</h1>
+    
 }
 function Amor(){
     return <h1>Amor, agradeço voce por tudo, sem vc eu não estaria onde estou agora, quero continuar construindo uma vida inteira com vc meu amor Te amo muito!!!!</h1>
@@ -9,4 +10,5 @@ function Agu(){
     return <h1>Agu, vc é a melhor coisa que aconteceu na minha vida, vc é minha filha, minha tudo, te amo muito minha filha!!!!</h1>
 }
 
-export default {Home, Amor, Agu}
+export default Home
+export {Amor, Agu}
