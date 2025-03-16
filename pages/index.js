7d478dbca22e8c22517faa026399ba174a3c6817
Lotar-ursudev <145760350+Lotar-ursulino@users.noreply.github.com>
2025-03-16 22:26:48 +0000

    teste frase

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     return <h1>Amor,Agu, Amo muito vcs duas!!!!</h1>
 }
 function Amor(){
-    return <h1>Amor, agradeço voce por tudo, sem vc eu não estaria onde estou, quero continuar construindo uma vida inteira com vc meu amor Te amo muito!!!!</h1>
+    return <h1>Amor, agradeço voce por tudo, sem vc eu não estaria onde estou agora, quero continuar construindo uma vida inteira com vc meu amor Te amo muito!!!!</h1>
 }
 
 function Agu(){
