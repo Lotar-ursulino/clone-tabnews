595788e27e9a9a65f77a3159231af0f5afb16d62
Lotar-ursudev <145760350+Lotar-ursulino@users.noreply.github.com>
2025-03-26 11:13:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 projeto de programação para treino para http://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
-function Home(){
-    var straing1 = "Amor,Agu, Amo muito vcs dua!!!!";
-    var straing2 = "Amor,eu amo muito voce, quero continuar construindo uma vida inteira com vc, sou muito feliz ao seu lado meu amor";
-    var straing3 = "Filha, eu te amo muito, vc é a razão da minha vida, voce e meu tudo, amo muito vc";
-    return (
-      <div>
-        <p>{straing1}</p>
-        <p>{straing2}</p>
-        <p>{straing3}</p>
-      </div>
-    );      
-    
+function Home() {
+  var straing1 = "Amor,Agu, Amo muito vcs dua!!!!";
+  var straing2 =
+    "Amor,eu amo muito voce, quero continuar construindo uma vida inteira com vc, sou muito feliz ao seu lado meu amor";
+  var straing3 =
+    "Filha, eu te amo muito, vc é a razão da minha vida, voce e meu tudo, amo muito vc";
+  return (
+    <div>
+      <p>{straing1}</p>
+      <p>{straing2}</p>
+      <p>{straing3}</p>
+    </div>
+  );
 }
- export default Home;
+export default Home;
