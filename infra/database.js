51e5b8f64d41e3650aa9925f9861cd41c42bc9c2
Lotar-ursudev <145760350+Lotar-ursulino@users.noreply.github.com>
2025-05-28 20:34:11 +0000

    teste certo da aula 18

--- infra/database.js
+++ infra/database.js
@@ -4,7 +4,7 @@ async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    user: process.env.PGUSER,
+    user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
