05db6ec2771370d78fdae6d5c134f63474e7027c
Lotar-ursudev <145760350+Lotar-ursulino@users.noreply.github.com>
2025-07-27 12:37:31 +0000

    database, conexão maxima e conexões abertas

--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -3,5 +3,13 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+  console.log(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("17.5");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+
 });
